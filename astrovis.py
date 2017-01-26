5d74a0fd1b211c1dc8158df3b91f6e428b0a100f
Giovanni De Cesare <decesare.giovanni@gmail.com>
2017-01-26 15:22:28 +0100

    Plot plantes and stars

--- astrovis.py
+++ astrovis.py
@@ -1,8 +1,42 @@
+from astropy.io import ascii
+import pylab
+
 def info():
     """
     Description: Print some infos
 
     Usage: info()
     """
-    print "Some tool to visualize the result." 
+    print "Some tool to visualize the results."
+    pass
+
+def plot(fileName):
+    """
+    Description: plot the orbit
+
+    Usage: plot(fileName)
+    """
+    planetTrajectory = ascii.read(fileName)
+    x = planetTrajectory['col3']
+    y = planetTrajectory['col4']
+    pylab.plot(x, y, 'b', label='Planet orbit')
+    pylab.show()
+    pass
+
+def plot_planet():
+    """
+    Description: plot the planetary orbit
+
+    Usage: plot_planet()
+    """
+    plot('planet.dat')
+    pass
+
+def plot_star():
+    """
+    Description: plot the orbit of the secondary star
+
+    Usage: plot_star()
+    """
+    plot('star.dat')
     pass
